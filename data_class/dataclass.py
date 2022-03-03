0b977995e9a7d2f0b0debc4e25508d5104fdce3b
Flynn Owen <flynnoo49@gmail.com>
2022-03-03 14:57:44 +1300

    add dataclass inheritence

--- data_class/dataclass.py
+++ data_class/dataclass.py
@@ -1,12 +1,13 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 class Human1:
 
-    def __init__(self, name: str, age: int, gender: str):
-        self.name   = name
-        self.age    = age
-        self.gender = gender
+    def __init__(self, name: str, age: int, gender: str, species: str):
+        self.name    =  name
+        self.age     =  age
+        self.gender  =  gender
+        self.species = species
 
     def say_name(self):
         print('Hi, my name is ' + self.name)
@@ -14,26 +15,45 @@ class Human1:
 
 @dataclass
 class Human2:
-    name  : str
-    age   : int
-    gender: str
+    name   : str
+    age    : int
+    gender : str
+    species: str
 
     def say_name(self):
         print('Hi, my name is ' + self.name)
 
 
-flynn = Human1('Flynn', 25, 'Male')
+@dataclass
+class Developer(Human2):
+    occupation: str
+    species: str = field(default='Nerd', init=False) # Overwrites the inherited value, must use field for default
+
+
+flynn = Human1('Flynn', 25, 'Male', 'Human')
 print(type(flynn))
 print(flynn)
 print(flynn.name)
 print(flynn.age)
 print(flynn.gender)
+print(flynn.species)
 flynn.say_name()
 
-flynn = Human2('Flynn', 25, 'Male')
+flynn = Human2('Flynn', 25, 'Male', 'Human')
 print(type(flynn))
 print(flynn) # Comes with built-in __repr__ method
 print(flynn.name)
 print(flynn.age)
 print(flynn.gender)
+print(flynn.species)
+flynn.say_name()
+
+flynn = Developer('Flynn', 25, 'Male', 'Developer')
+print(type(flynn))
+print(flynn) # Comes with built-in __repr__ method
+print(flynn.name)
+print(flynn.age)
+print(flynn.gender)
+print(flynn.species)
+print(flynn.occupation)
 flynn.say_name()
