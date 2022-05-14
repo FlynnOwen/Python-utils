7d720c6654aba69115b5bc5cdfd8052d93a438b6
Flynn Owen <flynnoo49@gmail.com>
2022-05-14 23:29:05 +1200

    customize log with current time

--- logging/logger.py
+++ logging/logger.py
@@ -1,9 +1,13 @@
+'''
+Writing to a logging file through the duration of a script.
+'''
 import logging
+from datetime import datetime
 
 # Define a file to write the logs to. Level is the minimum level written.
 # Logs are appended to the file by default (filemode = 'a')
 # Use filemode = 'w' to overwrite the log
-logging.basicConfig(filename='test_log.log', level=logging.WARNING, filemode='w')
+logging.basicConfig(filename=str(datetime.today()) + '.log', level=logging.DEBUG, filemode='w')
 
 # debug is lowest level
 logging.debug('This is a debug message')
